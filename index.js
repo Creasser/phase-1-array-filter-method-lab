50a9c842a2de25d76a82bb8e3b050dfeb2e4b5e7
Blake Creasser <blakecreasser@aggienetwork.com>
2022-11-06 22:50:42 -0600

    commit tree c57efc70d7a50984b0cb50dcd1f5b749f2a5ce5d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(arr, string){
+   return arr.filter((name) => name.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(arr, string){
+    return arr.filter((letter) => letter[0] === string[0])
+}
+
+function matchName(arr, string){
+    return arr.filter((item) => item.name === string)
+}
+
+
+//
